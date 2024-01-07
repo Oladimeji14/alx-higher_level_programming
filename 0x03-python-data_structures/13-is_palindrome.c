96ed7640b64b22096314ec073e998e2d88cc69b4
Oladimeji14 <demix1495@gmail.com>
2024-01-07 06:54:09 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,58 +1,50 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: head of listint_t
- * @n: int to add in listint_t list
- * Return: address of the new element, or NULL if it failed
- */
-listint_t *add_nodeint(listint_t **head, const int value)
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new_node;
-
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->next = *head;
-    *head = new_node;
-    return (new_node);
+        listint_t *new;
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = n;
+        new->next = *head;
+        *head = new;
+        return (new);
 }
-
 /**
- * is_palindrome - identify if a single linked list is palindrome
- * @head: head of listint_t
- * Return: 1 if it is palindrome else 0
- */
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-    listint_t *temp_head = *head;
-    listint_t *reversed_list = NULL, *temp_list = NULL;
-
-    if (*head == NULL || temp_head->next == NULL)
-        return (1);
-    
-    while (temp_head != NULL)
-    {
-        add_nodeint(&reversed_list, temp_head->n);
-        temp_head = temp_head->next;
-    }
-
-    temp_list = reversed_list;
-
-    while (*head != NULL)
-    {
-        if ((*head)->n != temp_list->n)
+        listint_t *head2 = *head;
+        listint_t *aux = NULL, *aux2 = NULL;
+        if (*head == NULL || head2->next == NULL)
+                return (1);
+        while (head2 != NULL)
         {
-            free_listint(reversed_list);
-            return (0);
+                add_nodeint(&aux, head2->n);
+                head2 = head2->next;
         }
-        *head = (*head)->next;
-        temp_list = temp_list->next;
-    }
-
-    free_listint(reversed_list);
-    return (1);
+        aux2 = aux;
+        while (*head != NULL)
+        {
+                if ((*head)->n != aux2->n)
+                {
+                        free_listint(aux);
+                        return (0);
+                }
+                *head = (*head)->next;
+                aux2 = aux2->next;
+        }
+        free_listint(aux);
+        return (1);
 }
