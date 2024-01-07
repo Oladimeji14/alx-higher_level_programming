d6b729248b127bd17538fac3c0bce1b0a96294c8
Oladimeji14 <demix1495@gmail.com>
2024-01-07 06:51:15 -0800

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: head of listint_t
+ * @n: int to add in listint_t list
+ * Return: address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int value)
+{
+    listint_t *new_node;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n = value;
+    new_node->next = *head;
+    *head = new_node;
+    return (new_node);
+}
+
+/**
+ * is_palindrome - identify if a single linked list is palindrome
+ * @head: head of listint_t
+ * Return: 1 if it is palindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *temp_head = *head;
+    listint_t *reversed_list = NULL, *temp_list = NULL;
+
+    if (*head == NULL || temp_head->next == NULL)
+        return (1);
+    
+    while (temp_head != NULL)
+    {
+        add_nodeint(&reversed_list, temp_head->n);
+        temp_head = temp_head->next;
+    }
+
+    temp_list = reversed_list;
+
+    while (*head != NULL)
+    {
+        if ((*head)->n != temp_list->n)
+        {
+            free_listint(reversed_list);
+            return (0);
+        }
+        *head = (*head)->next;
+        temp_list = temp_list->next;
+    }
+
+    free_listint(reversed_list);
+    return (1);
+}
