654667a98553f469d71051a390ff5f4ee82fdbd1
Oladimeji14 <demix1495@gmail.com>
2024-01-07 06:21:11 -0800

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
+    length = len(sentence) if sentence else 0
+    return (length, sentence[:1] if sentence else None)
